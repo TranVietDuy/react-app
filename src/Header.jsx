5acb973c3bda5dfb72cc8db70a9ceac99dceccd2
Duy Tran <duytran840@learnit.fi>
2022-09-07 15:52:54 +0300

    add style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react"
 
 const Header = () => {
-  return (
-    <h1>Groceries List</h1>
-  )
+  
+  return <header >Groceries List</header>
 }
 
 export default Header
